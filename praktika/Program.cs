2410788fd346a8b6daa9217892d29ffce2af131d
user <user@DESKTOP-FLHUGJ1>
2024-04-16 00:57:09 +0300

    Добавьте файлы проекта.

---
+++ praktika/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод цены за килограмм и веса яблок
+        Console.Write("Введите цену одного килограмма яблок (руб.): ");
+        double pricePerKg = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите вес яблок (кг): ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        // Вычисление стоимости покупки
+        double cost = pricePerKg * weight;
+
+        // Вывод результата
+        Console.WriteLine($"Стоимость покупки: {cost} руб.");
+        Console.ReadLine();
+    }
+}
