1d2b4f4e30bfc69200a1ed1bd552f05af285d613
user <user@DESKTOP-FLHUGJ1>
2024-04-18 11:15:01 +0300

    добавлено

---
+++ zadanie3/zadanie3/task5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение h: ");
+        double h = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("x\tf(x)");
+
+        for (double x = a; x <= b; x += h)
+        {
+            double fx = x * x + 2 * x;
+            Console.WriteLine($"{x}\t{fx}");
+            Console.ReadLine();
+        }
+    }
+}
