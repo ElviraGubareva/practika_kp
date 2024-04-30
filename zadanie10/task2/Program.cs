48429d12c76684cedc5732abe58e6c377fb39e87
user <user@DESKTOP-FLHUGJ1>
2024-04-30 10:53:15 +0300

    сдано

---
+++ zadanie10/task2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+public class A
+{
+    protected int a = 21;
+    protected int b = 21;
+
+    public int C
+    {
+        get
+        {
+            int result = a + b;
+            while (result > 0)
+            {
+                result--;
+            }
+            return result;
+        }
+    }
+}
+
+public class B : A
+{
+    private int d = 21;
+
+    public B() : base()
+    {
+    }
+
+    public B(int d)
+    {
+        this.d = d;
+    }
+
+    public int C2
+    {
+        get
+        {
+            int result = a + b + d;
+            while (result > 0)
+            {
+                result--;
+            }
+            return result;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        A a = new A();
+        Console.WriteLine("Значение свойства C класса A: " + a.C);
+
+        B b1 = new B();
+        Console.WriteLine("Значение свойства C2 класса B (конструктор по умолчанию): " + b1.C2);
+
+        B b2 = new B(42);
+        Console.WriteLine("Значение свойства C2 класса B (конструктор с параметром): " + b2.C2);
+        Console.ReadLine();
+    }
+}
