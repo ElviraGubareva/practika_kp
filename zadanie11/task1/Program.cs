d3c560833f0e9940d421cf817b8236e7857bcf49
user <user@DESKTOP-FLHUGJ1>
2024-05-01 14:28:35 +0300

    сделано

---
+++ zadanie11/task1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+public abstract class Clothing
+{
+    public string Name { get; set; }
+
+    public Clothing(string name)
+    {
+        Name = name;
+    }
+
+    public abstract double FabricConsumption();
+}
+
+public class Coat : Clothing
+{
+    public double V { get; set; }
+
+    public Coat(string name, double v) : base(name)
+    {
+        V = v;
+    }
+
+    public override double FabricConsumption()
+    {
+        return V / 6.5 + 0.5;
+    }
+}
+
+public class Suit : Clothing
+{
+    public double H { get; set; }
+
+    public Suit(string name, double h) : base(name)
+    {
+        H = h;
+    }
+
+    public override double FabricConsumption()
+    {
+        return 2 * H + 0.3;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Clothing[] clothes = new Clothing[5]
+        {
+            new Coat("Coat1", 10),
+            new Suit("Suit1", 20),
+            new Coat("Coat2", 15),
+            new Suit("Suit2", 25),
+            new Coat("Coat3", 20)
+        };
+
+        foreach (var item in clothes)
+        {
+            Console.WriteLine($"Название: {item.Name}, Расход ткани: {item.FabricConsumption()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ zadanie11/task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+abstract class Bird
+{
+    public abstract int FoodNeeded();
+}
+
+class Stork : Bird
+{
+    public override int FoodNeeded()
+    {
+        return 5; // количество пищи, необходимое аисту
+    }
+}
+
+class Crow : Bird
+{
+    public override int FoodNeeded()
+    {
+        return 3; // количество пищи, необходимое вороне
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bird[] birds = new Bird[5];
+        birds[0] = new Stork();
+        birds[1] = new Crow();
+        birds[2] = new Stork();
+        birds[3] = new Crow();
+        birds[4] = new Stork();
+
+        for (int i = 0; i < birds.Length; i++)
+        {
+            Console.WriteLine($"Птица {i + 1} требует {birds[i].FoodNeeded()} единиц пищи.");
+            Console.ReadLine();
+        }
+    }
+}
