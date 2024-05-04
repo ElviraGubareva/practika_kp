55ea1464614d03b85af30e59e8b513c54e35c1f0
user <user@DESKTOP-FLHUGJ1>
2024-05-04 10:56:28 +0300

    сделано

---
+++ zadanie14/task3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+class Program
+{
+    static object lockObject = new object();
+    static void Main()
+    {
+        Console.Write("Введите значение A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        Thread t1 = new Thread(() => Sum(A, N));
+        Thread t2 = new Thread(() => Sum(A, N));
+        t1.Start();
+        t2.Start();
+        t1.Join();
+        t2.Join();
+        Thread t3 = new Thread(() => Multiply(A, N));
+        t3.Start();
+        t3.Join();
+
+        Console.ReadLine();
+    }
+    static void Sum(int A, int N)
+    {
+        int sum = A;
+        for (int i = 1; i <= N; i++)
+        {
+            sum += A;
+        }
+        Console.WriteLine($"Сумма: {sum}");
+    }
+    static void Multiply(int A, int N)
+    {
+        lock (lockObject)
+        {
+            int product = A;
+            for (int i = 1; i <= N; i++)
+            {
+                product *= A;
+            }
+            Console.WriteLine($"Произведение: {product}");
+        }
+    }
+}
---
+++ zadanie14/task4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = Enumerable.Range(0, 100).ToArray(); 
+        int threadCount = 4; 
+
+        int[] partialSums = new int[threadCount];
+        Thread[] threads = new Thread[threadCount];
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            int threadIndex = i;
+            threads[i] = new Thread(() =>
+            {
+                int start = threadIndex * numbers.Length / threadCount;
+                int end = (threadIndex + 1) * numbers.Length / threadCount;
+                for (int j = start; j < end; j++)
+                {
+                    if (numbers[j] % 2 == 0)
+                    {
+                        partialSums[threadIndex] += numbers[j];
+                    }
+                }
+            });
+
+            threads[i].Start();
+        }
+
+        foreach (Thread thread in threads)
+        {
+            thread.Join();
+        }
+
+        int totalSum = partialSums.Sum();
+
+        Console.WriteLine("Сумма четных чисел: " + totalSum);
+        Console.ReadLine();
+    }
+}
