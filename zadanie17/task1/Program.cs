d8bfb24983904c43a51f68f82c19c03cc36648c5
user <user@DESKTOP-FLHUGJ1>
2024-05-07 09:01:46 +0300

    сделано

---
+++ zadanie17/task1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace GeometryLibrary
+{
+    public class Triangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public bool Exists()
+        {
+            return A + B > C && A + C > B && B + C > A;
+        }
+
+        public double Perimeter()
+        {
+            return A + B + C;
+        }
+
+        public double Area()
+        {
+            double p = Perimeter() / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        public string Type()
+        {
+            if (A == B && B == C)
+                return "Равносторонний";
+            else if (A == B || B == C || A == C)
+                return "Равнобедренный";
+            else
+                return "Разносторонний";
+        }
+    }
+
+    public class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GeometryLibrary.Triangle triangle = new GeometryLibrary.Triangle(3, 4, 5);
+        if (triangle.Exists())
+        {
+            Console.WriteLine($"Периметр треугольника: {triangle.Perimeter()}");
+            Console.WriteLine($"Площадь треугольника: {triangle.Area()}");
+            Console.WriteLine($"Тип треугольника: {triangle.Type()}");
+        }
+        else
+        {
+            Console.WriteLine("Треугольник с заданными сторонами не существует.");
+        }
+
+        GeometryLibrary.Rectangle rectangle = new GeometryLibrary.Rectangle(4, 5);
+        Console.WriteLine($"Периметр прямоугольника: {rectangle.Perimeter()}");
+        Console.WriteLine($"Площадь прямоугольника: {rectangle.Area()}");
+        Console.ReadLine();
+    }
+}
---
+++ zadanie17/task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FlowerLibrary
+{
+    public class Flower
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public Flower(string name, string color)
+        {
+            Name = name;
+            Color = color;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Цветок: {Name}, Цвет: {Color}");
+        }
+    }
+
+    public class Rose : Flower
+    {
+        public Rose(string color) : base("Роза", color) { }
+    }
+
+    public class Tulip : Flower
+    {
+        public Tulip(string color) : base("Тюльпан", color) { }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FlowerLibrary.Rose rose = new FlowerLibrary.Rose("Красный");
+        rose.DisplayInfo();
+
+        FlowerLibrary.Tulip tulip = new FlowerLibrary.Tulip("Желтый");
+        tulip.DisplayInfo();
+        Console.ReadLine();
+    }
+}
