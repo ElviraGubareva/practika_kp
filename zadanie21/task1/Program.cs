de4c5aabc277a712e499aba2d25ec9efc4d58e7c
user <user@DESKTOP-FLHUGJ1>
2024-05-11 08:33:56 +0300

    сделано

---
+++ zadanie21/task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public interface IStrategy
+{
+    void Execute();
+}
+
+public class ConcreteStrategyA : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Выполнена стратегия A");
+    }
+}
+
+public class ConcreteStrategyB : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Выполнена стратегия B");
+    }
+}
+
+public class Context
+{
+    private IStrategy _strategy;
+
+    public Context(IStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void SetStrategy(IStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void ExecuteStrategy()
+    {
+        _strategy.Execute();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Context context = new Context(new ConcreteStrategyA());
+        context.ExecuteStrategy();
+
+        context.SetStrategy(new ConcreteStrategyB());
+        context.ExecuteStrategy();
+
+        Console.ReadKey();
+        Console.ReadLine();
+    }
+}
