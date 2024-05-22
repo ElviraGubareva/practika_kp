c59cb18c4328f6b0604c7aa69ca94bb480765215
user <user@DESKTOP-FLHUGJ1>
2024-05-22 11:31:26 +0300

    сделано

---
+++ zadanie24/task1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            spData.Text = Convert.ToString(System.DateTime.Today.ToLongDateString());
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text;
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    spWin.Text = "Windows is cascade";
+                    break;
+                case "Title":
+                    this.LayoutMdi
+                    (System.Windows.Forms.MdiLayout.TileHorizontal);
+                    spWin.Text = "Windows is horizontal";
+                    break;
+            }
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ zadanie25/task3/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Definitions.Charts;
+using LiveCharts.Wpf;
+namespace z2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public ChartValues<double> Values1 { get; set; }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SeriesCollection series = new SeriesCollection();
+                double xMin = double.Parse(textboxXmin.Text);
+                double xMax = double.Parse(Xmax.Text);
+                double step = double.Parse(Step.Text);
+                ChartValues<double> cv1 = new ChartValues<double>();
+                ChartValues<double> cv2 = new ChartValues<double>();
+                double coord = 0;
+                for (double i = xMin; i <= xMax; i += step)
+                {
+                    double result = Math.Pow(i, 3);
+                    cv2.Add(coord++);
+                    cv1.Add(result);
+
+                }
+                LineSeries line = new LineSeries();
+                line.Values = cv1;
+                series.Add(line);
+                cartChart.Series = series;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void cartChart_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
